9a3c698c86002b0c3346ecdbc3a762facaf97c3d
CrocoKyle <me@kyle-dixon.com>
2021-07-15 04:54:53 -0400

    Adding Xeoma installation and config to xeoma.sh

--- xeoma.sh
+++ xeoma.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
 wget https://felenasoft.com/xeoma/downloads/latest/linux/xeoma_linux_arm7.tgz
+tar -xvf xeoma_linux_arm7.tgz
+./xeoma.app -install -coreauto
+echo ""
+echo "Complete!"
+echo "Please update OpenGL drivers to prevent CPU issues"
+echo "Start by running 8 Update"
+echo "Next, navigate to Advanced Options > GL Driver > GL (Full KMS) Open GL desktop driver with full KMS"
+sudo raspi-config
+echo ""
+echo "Please reboot your pi before running the next install script"
