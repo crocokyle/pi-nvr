1fe2bfd0bb2a1c0af3f90e84ad31f5e082f9b697
CrocoKyle <me@kyle-dixon.com>
2021-07-15 05:09:58 -0400

    Splitting installation script for reboot

--- install-2.sh
+++ install-2.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+# Prerequisites
+sudo snap install core
+sudo snap install gnome-system-monitor
+sudo apt-get install libglu1-mesa htop 
 wget https://felenasoft.com/xeoma/downloads/latest/linux/xeoma_linux_arm7.tgz
 tar -xvf xeoma_linux_arm7.tgz
 ./xeoma.app -install -coreauto
