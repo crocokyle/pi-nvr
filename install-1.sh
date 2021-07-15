40b37259a2f517738165f2359c7fb576ad5d49db
CrocoKyle <me@kyle-dixon.com>
2021-07-15 06:07:36 -0400

    Adding hostname updates and testing repo

--- install-1.sh
+++ install-1.sh
@@ -1,11 +1,22 @@
 #!/bin/bash
+
+# Change hostname
+sudo cp hosts /etc/hosts
+sudo cp hostname /etc/hostname
+
+# Update to use testing
+sudo cp sources.list /etc/apt/sources.list
+
 sudo apt -y update
-sudo apt -y upgrade
+sudo apt -y full-upgrade
 
 # Teamviewer setup
 wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb
 sudo dpkg -i teamviewer-host_armhf.deb
 sudo apt --fix-broken install
+
+# ddclient setup
+sudo apt-get install ddclient
 
 sudo apt install snapd
 echo ""
--- install-2.sh
+++ install-2.sh
@@ -4,7 +4,7 @@ sudo snap install core
 sudo snap install gnome-system-monitor
 cp Resource\ Monitor.desktop ~/Desktop/.
 
-sudo apt-get install libglu1-mesa htop 
+sudo apt-get install libglu1-mesa htop ddclient
 wget https://felenasoft.com/xeoma/downloads/latest/linux/xeoma_linux_arm7.tgz
 tar -xvf xeoma_linux_arm7.tgz
 ./xeoma.app -install -coreauto
