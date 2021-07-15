a836450284eff190611ebd572365a049f85a6ecb
Crocokyle <dixonkyleard@gmail.com>
2021-07-14 23:11:24 -1000

    Update install-1.sh

--- install-1.sh
+++ install-1.sh
@@ -3,4 +3,13 @@ sudo apt update
 sudo apt upgrade
 
 sudo apt install snapd
+echo ""
+echo "Complete!"
+echo "Please update OpenGL drivers to prevent CPU issues"
+echo "Start by running 8 Update"
+echo "Next, navigate to Advanced Options > GL Driver > GL (Fake KMS)"
+read -p "Press ENTER to open raspi-config"
+sudo raspi-config
 
+read -p "Next, we will reboot. After the reboot, please run install-2.sh. Press ENTER to reboot now."
+sudo reboot
