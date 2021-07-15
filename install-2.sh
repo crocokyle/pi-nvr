9c1e9c321c3362e604341f406eebeef63d662935
CrocoKyle <me@kyle-dixon.com>
2021-07-15 05:29:05 -0400

    Update resource monitor shortcut

--- install-2.sh
+++ install-2.sh
@@ -2,6 +2,8 @@
 # Prerequisites
 sudo snap install core
 sudo snap install gnome-system-monitor
+cp Resource\ Monitor.desktop ~/Desktop/.
+
 sudo apt-get install libglu1-mesa htop 
 wget https://felenasoft.com/xeoma/downloads/latest/linux/xeoma_linux_arm7.tgz
 tar -xvf xeoma_linux_arm7.tgz
