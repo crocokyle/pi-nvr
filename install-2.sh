339008ed19fb6c2eee0cc3a0703adba6e11981e3
crocokyle <me@kyle-dixon.com>
2021-07-15 23:08:25 -0400

    Update comments

--- install-2.sh
+++ install-2.sh
@@ -3,8 +3,9 @@
 sudo snap install core
 sudo snap install gnome-system-monitor
 cp conf/Resource\ Monitor.desktop ~/Desktop/.
-
 sudo apt-get install libglu1-mesa htop ddclient
+
+# Xeoma installation
 wget https://felenasoft.com/xeoma/downloads/latest/linux/xeoma_linux_arm7.tgz
 tar -xvf xeoma_linux_arm7.tgz
 ./xeoma.app -install -coreauto
