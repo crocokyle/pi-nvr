e1854a4365d4da11575d2f5425c719dae62e69db
Crocokyle <dixonkyleard@gmail.com>
2021-07-15 18:43:12 -1000

    Remove ddclient

--- install-2.sh
+++ install-2.sh
@@ -3,7 +3,7 @@
 sudo snap install core
 sudo snap install gnome-system-monitor
 cp conf/Resource\ Monitor.desktop ~/Desktop/.
-sudo apt-get install libglu1-mesa htop ddclient
+sudo apt-get install libglu1-mesa htop
 
 # Xeoma installation
 wget https://felenasoft.com/xeoma/downloads/latest/linux/xeoma_linux_arm7.tgz
